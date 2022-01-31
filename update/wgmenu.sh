4e17e6f9ad8109b016cc52396b81d0566e9f596e
Seno Wahyu Hidayat <70935444+senowahyu62@users.noreply.github.com>
2022-01-31 10:19:30 +0700

    Update wgmenu.sh

--- update/wgmenu.sh
+++ update/wgmenu.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+clear
+m="\033[0;1;36m"
+y="\033[0;1;37m"
+yy="\033[0;1;32m"
+yl="\033[0;1;33m"
+wh="\033[0m"
 echo -e "$y                          WIREGUARD $wh"
 echo -e "$y-------------------------------------------------------------$wh"
-echo -e "$yy 1$y.Create Account Wireguard"
-echo -e "$yy 2$y.Delete Account Wireguard"
-echo -e "$yy 3$y.Extending Account Wireguard Active Life"
+echo -e "$yy 1$y. Create Account Wireguard"
+echo -e "$yy 2$y. Delete Account Wireguard"
+echo -e "$yy 3$y. Extending Account Wireguard Active Life"
 echo -e "$yy 4$y. Menu"
 echo -e "$yy 5$y. Exit"
 echo -e "$y-------------------------------------------------------------$wh
