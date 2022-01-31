dd8252417bca6053caf8f86c4dc41a0ed6dadc2e
Seno Wahyu Hidayat <70935444+senowahyu62@users.noreply.github.com>
2022-01-31 13:51:12 +0700

    Update wgmenu.sh

--- update/wgmenu.sh
+++ update/wgmenu.sh
@@ -12,7 +12,7 @@ echo -e "$yy 2$y. Delete Account Wireguard"
 echo -e "$yy 3$y. Extending Account Wireguard Active Life"
 echo -e "$yy 4$y. Menu"
 echo -e "$yy 5$y. Exit"
-echo -e "$y-------------------------------------------------------------$wh
+echo -e "$y-------------------------------------------------------------$wh"
 read -p "Select From Options [ 1 - 5 ] : " menu
 echo -e ""
 case $menu in
