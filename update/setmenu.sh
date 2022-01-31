a89cfc8dc2eaa4e46db06a8f3e2663dfba2bac9f
Seno Wahyu Hidayat <70935444+senowahyu62@users.noreply.github.com>
2022-01-31 10:11:37 +0700

    Update setmenu.sh

--- update/setmenu.sh
+++ update/setmenu.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#!/bin/bash
+clear
+m="\033[0;1;36m"
+y="\033[0;1;37m"
+yy="\033[0;1;32m"
+yl="\033[0;1;33m"
+wh="\033[0m"
 echo -e "$y                   SYSTEM SETTING$wh"
 echo -e "$y-------------------------------------------------------------$wh"
 echo -e "$yy 1$y.  Add Or Change Subdomain Host For VPS"
