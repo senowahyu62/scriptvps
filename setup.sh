6314c7b40823d76377ac57cd4e2fb11e6dac49fd
Seno Wahyu Hidayat <70935444+senowahyu62@users.noreply.github.com>
2022-01-31 11:47:36 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -44,16 +44,7 @@ akbarvpnnnnnnnnnn="raw.githubusercontent.com/senowahyu62/scriptvps/main/ohp"
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 IZIN=$(wget -qO- ipinfo.io/ip);
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Facebook : https://m.facebook.com/lis.tio.718"
-echo -e "${NC}${LIGHT}WhatsApp : 081545854516"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/Akbar218"
-exit 0
-fi
+
 rm -f setup.sh
 clear
 if [ -f "/etc/xray/domain" ]; then
