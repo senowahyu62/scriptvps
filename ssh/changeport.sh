ffb772e2d3dbdfb1e44d3d817d6b718a7b817ee2
Seno Wahyu Hidayat <70935444+senowahyu62@users.noreply.github.com>
2022-01-31 16:35:26 +0700

    Update changeport.sh

--- ssh/changeport.sh
+++ ssh/changeport.sh
@@ -11,20 +11,7 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/senowahyu62/perizinan/main/ipvps.txt | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Facebook : https://m.facebook.com/lis.tio.718"
-echo -e "${NC}${LIGHT}WhatsApp : 081545854516"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/Akbar218"
-exit 0
-fi
+
 clear
 echo -e ""
 echo -e "======================================"
@@ -63,10 +50,10 @@ portsquid
 portsstp
 ;;
 7)
-portsshws
+portsshnontls
 ;;
 8)
-portsshnontls
+portsshws
 ;;
 9)
 clear
