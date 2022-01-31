b2dcca9b4135e7ae3fad32196e25eb1b67999c9a
Seno Wahyu Hidayat <70935444+senowahyu62@users.noreply.github.com>
2022-01-31 10:16:59 +0700

    Update sstpmenu.sh

--- update/sstpmenu.sh
+++ update/sstpmenu.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+clear
+m="\033[0;1;36m"
+y="\033[0;1;37m"
+yy="\033[0;1;32m"
+yl="\033[0;1;33m"
+wh="\033[0m"
 echo -e "$y                                SSTP $wh"
 echo -e "$y-------------------------------------------------------------$wh"
-echo -e "$yy 1$y.Create Account SSTP"
-echo -e "$yy 2$y.Delete Account SSTP"
-echo -e "$yy 3$y.Extending Account SSTP Active Life"
-echo -e "$yy 4$y.Check User Login SSTP
+echo -e "$yy 1$y. Create Account SSTP"
+echo -e "$yy 2$y. Delete Account SSTP"
+echo -e "$yy 3$y. Extending Account SSTP Active Life"
+echo -e "$yy 4$y. Check User Login SSTP
 echo -e "$yy 5$y. Menu"
 echo -e "$yy 6$y. Exit"
 echo -e "$y-------------------------------------------------------------$wh
