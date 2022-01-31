dcce418d90a0737f4192de8b3e3e5cf162686054
Seno Wahyu Hidayat <70935444+senowahyu62@users.noreply.github.com>
2022-01-31 13:54:01 +0700

    Update sstpmenu.sh

--- update/sstpmenu.sh
+++ update/sstpmenu.sh
@@ -13,7 +13,7 @@ echo -e "$yy 3$y. Extending Account SSTP Active Life"
 echo -e "$yy 4$y. Check User Login SSTP
 echo -e "$yy 5$y. Menu"
 echo -e "$yy 6$y. Exit"
-echo -e "$y-------------------------------------------------------------$wh
+echo -e "$y-------------------------------------------------------------$wh"
 read -p "Select From Options [ 1 - 6 ] : " menu
 echo -e ""
 case $menu in
