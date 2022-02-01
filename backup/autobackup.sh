b0e7f2868f38ec7ee8714a50a3e35e81c980f630
Seno Wahyu Hidayat <70935444+senowahyu62@users.noreply.github.com>
2022-02-01 12:57:01 +0700

    Update autobackup.sh

--- backup/autobackup.sh
+++ backup/autobackup.sh
@@ -13,6 +13,8 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 clear
+IP=$(wget -qO- ipinfo.io/ip);
+date=$(date +"%Y-%m-%d");
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[ON]${Font_color_suffix}"
 Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
